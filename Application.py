324d8f7c84b6c4667849fc51591d06664f195a57
GuilhermeAlmeidaDev <guilhermerocha124@gmail.com>
2021-02-16 14:20:33 -0300

    iniciando projeto de estudos de Python

---
+++ Application.py
@@ -0,0 +1,14 @@
+anniYear = 1
+perName = ["Bob", "Marry", "Jack", "Charlie", "Frank", "Katy"]
+
+print("Hi", perName[0],", today", anniYear, "th anniversary of SS Academy")
+
+print("Hi", perName[1],", today", anniYear, "th anniversary of SS Academy")
+
+print("Hi", perName[2],", today", anniYear, "th anniversary of SS Academy")
+
+print("Hi", perName[3],", today", anniYear, "th anniversary of SS Academy")
+
+print("Hi", perName[4],", today", anniYear, "th anniversary of SS Academy")
+
+print("Hi", perName[5],", today", anniYear, "th anniversary of SS Academy")
